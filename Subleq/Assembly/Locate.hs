8c5a2ff52fd7032ee5bba8ffdb335cc748969aa4
na4zagin3 <mrty.ityt.pt@gmail.com>
2015-01-18 17:44:09 +0900

    add loader

--- Subleq/Assembly/Locate.hs
+++ Subleq/Assembly/Locate.hs
@@ -52,4 +52,34 @@ locate ma i o@(Subroutine _ args es) = Just (substituteObject sub o, next)
 
 locate _  _ (Macro {}) = Nothing
 
+locateModulePacked :: MemoryArchitecture m -> Integer -> Module -> (Integer, Map Id (Integer, Object))
+locateModulePacked ma initialAddr (Module mo) = M.foldrWithKey f (initialAddr, M.empty) mo
+    where
+      f :: (Id -> Object -> (Integer, Map Id (Integer, Object)) -> (Integer, Map Id (Integer, Object)))
+      f x obj (i, mp) = case locate ma i obj of
+                          Nothing -> (i, mp)
+                          Just (obj', i') -> (i', M.insert x (i, obj') mp)
+
+loadElement :: MemoryArchitecture m -> Integer -> Element -> m -> (m, Integer)
+loadElement ma i (ElemInst Subleq [(_, Number x)])                                  m = loadElement ma i (ElemInst Subleq $ map (\z->(Nothing, z)) [Number x, Number x]) m
+loadElement ma i (ElemInst Subleq [(_, Number x), (_, Number y)])                   m = loadElement ma i (ElemInst Subleq $ map (\z->(Nothing, z)) [Number x, Number y, Number (i + instructionLength ma)]) m
+loadElement ma i (ElemInst Subleq [(_, Number x), (_, Number y), (_, Number z)])    m = (writeWord ma i x $ writeWord ma (i + wl) y $ writeWord ma (i + 2 * wl) z m, i + 3 * wl)
+    where
+      wl = wordLength ma
+loadElement _  i (ElemLoc _) m = (m, i)
+loadElement _  i e@(SubroutineCall {})    _ = error $ printf "loadElement: addr %d: macro expansion (%s) is not expandable" i (show e)
+loadElement _  i e@(ElemInst {})    _ = error $ printf "loadElement: addr %d: instruction (%s) is not expandable" i (show e)
+
+loadElements :: MemoryArchitecture m -> Integer -> [Element] -> m -> m
+loadElements ma i elems m = fst $ Prelude.foldl (\(mem, next) el->loadElement ma next el mem) (m, i) elems
+
+loadObject :: MemoryArchitecture m -> Integer -> Object -> m -> m
+loadObject ma i (Subroutine _ _ elems) = loadElements ma i elems
+loadObject ma i (Macro _ _ elems)      = loadElements ma i elems
+
+loadModulePacked :: MemoryArchitecture m -> Integer -> Module -> m -> (Integer, Map Id Integer, m)
+loadModulePacked ma i mo mem = (end, allocation, M.foldr (uncurry $ loadObject ma) mem mao)
+    where
+      (end, mao) = locateModulePacked ma i mo
+      allocation = M.map fst mao
 
